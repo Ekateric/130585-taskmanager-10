47c0d51315ad77ee5e53272d6c7aa3c8f3252dbc
Ekaterina Kireeva <e.radakina@gmail.com>
2019-12-02 23:19:48 +0300

    Add filters mock & update filters template

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import {createMenuTemplate} from "./components/menu";
-import {createFilterTemplate} from "./components/filter";
+import {createFilterData} from "./mock/filters";
+import {createFiltersTemplate} from "./components/filter";
 import {createBoardTemplate} from "./components/board";
 import {createTasksData} from "./mock/tasks";
 import {createTaskTemplate} from "./components/task";
 import {createTaskEditTemplate} from "./components/task-edit";
 import {createLoadMoreButtonTemplate} from "./components/load-more-button";
 
-const TASK_COUNT = 3;
+const TASK_COUNT = 10;
+
+const tasks = createTasksData(TASK_COUNT);
+const filters = createFilterData(tasks);
 
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
@@ -16,13 +20,11 @@ const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
 render(siteHeaderElement, createMenuTemplate());
-render(siteMainElement, createFilterTemplate());
+render(siteMainElement, createFiltersTemplate(filters));
 render(siteMainElement, createBoardTemplate());
 
 const taskListElement = siteMainElement.querySelector(`.board__tasks`);
 render(taskListElement, createTaskEditTemplate());
-
-const tasks = createTasksData(TASK_COUNT);
 tasks.forEach((task) => render(taskListElement, createTaskTemplate(task)));
 
 const boardElement = siteMainElement.querySelector(`.board`);
