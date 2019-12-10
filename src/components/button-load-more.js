77b1677ae227fd6b16ff67e3b8e28c092cd45fcd
Ekaterina Kireeva <e.radakina@gmail.com>
2019-12-10 12:08:50 +0300

    Add ButtonLoadMoreView class

--- src/components/button-load-more.js
+++ src/components/button-load-more.js
@@ -1,5 +1,25 @@
-export const createButtonLoadMoreTemplate = () => {
-  return (
-    `<button class="load-more" type="button">load more</button>`
-  );
-};
+import {createElement} from "../helpers";
+
+const createButtonLoadMoreTemplate = () => `<button class="load-more" type="button">load more</button>`;
+
+export default class ButtonLoadMoreView {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createButtonLoadMoreTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
 import {Menu} from "./mock/menu";
+import {Filters} from "./mock/filters";
+import TasksMock from "./mock/tasks";
+
 import MenuModel from "./models/menu";
 import MenuView from "./components/menu";
-import {Filters} from "./mock/filters";
 import FiltersListModel from "./models/filters-list";
 import FiltersView from "./components/filters";
 import BoardView from "./components/board";
-import TasksMock from "./mock/tasks";
 import TasksListModel from "./models/tasks-list";
 import TaskView from "./components/task";
 import TaskFormView from "./components/task-form";
-import {createButtonLoadMoreTemplate} from "./components/button-load-more";
-import {getRandomInt, render, createElement} from "./helpers";
+import ButtonLoadMoreView from "./components/button-load-more";
+import {getRandomInt, render} from "./helpers";
 
 const TASK_COUNT = getRandomInt(1, 20);
 const TASK_PER_PAGE = 8;
@@ -41,7 +42,7 @@ tasks
 
 if (TASK_PER_PAGE < TASK_COUNT) {
   const boardElement = siteMainElement.querySelector(`.board`);
-  render(boardElement, createElement(createButtonLoadMoreTemplate()));
+  render(boardElement, new ButtonLoadMoreView().getElement());
 
   const loadMoreButton = boardElement.querySelector(`.load-more`);
   let showingTasksCount = TASK_PER_PAGE;
