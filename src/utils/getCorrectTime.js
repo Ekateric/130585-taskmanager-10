4365f34dfa79499849b1dbf083b461760f0edb82
Ekaterina Kireeva <e.radakina@gmail.com>
2019-12-17 12:02:20 +0300

    Add castTimeFormat util & refactor getCorrectTime util

--- src/utils/getCorrectTime.js
+++ src/utils/getCorrectTime.js
@@ -1,11 +1,16 @@
+import castTimeFormat from "./castTimeFormat";
 import MONTHS from "../data/months";
 
 export default (date) => {
-  const hours = date.getHours();
+  let hours = date.getHours();
+  const minutes = castTimeFormat(date.getMinutes());
+  const interval = hours < 12 ? `AM` : `PM`;
+
+  hours = castTimeFormat(hours % 12);
 
   return {
-    day: date.getDate(),
+    day: castTimeFormat(date.getDate()),
     month: MONTHS[date.getMonth()],
-    time: `${hours % 12}:${date.getMinutes()} ${hours < 12 ? `AM` : `PM`}`
+    time: `${hours}:${minutes} ${interval}`
   };
 };
