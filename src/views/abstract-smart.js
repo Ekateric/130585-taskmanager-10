dae91ef2522c2af229c07ea7ecc2d3b7c3c2fcf3
Ekaterina Kireeva <e.radakina@gmail.com>
2019-12-19 23:39:47 +0300

    Add AbstractSmart view

---
+++ src/views/abstract-smart.js
@@ -0,0 +1,20 @@
+import AbstractView from "./abstract";
+
+export default class AbstractSmartView extends AbstractView {
+  recoveryListeners() {
+    throw new Error(`Abstract method not implemented: recoveryListeners`);
+  }
+
+  rerender() {
+    const oldElement = this.getElement();
+    const parent = oldElement.parentElement;
+
+    this.removeElement();
+
+    const newElement = this.getElement();
+
+    parent.replaceChild(newElement, oldElement);
+
+    this.recoveryListeners();
+  }
+}
